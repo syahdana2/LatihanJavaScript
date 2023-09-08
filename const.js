d7f3cd65f4412a9fcbbe7f2e0dec2d2306d6cea3
OkanR1 <okansyahdana@gmail.com>
2023-09-08 20:32:52 +0700

    modifikasi

--- const.js
+++ const.js
@@ -1,2 +1,8 @@
+// variabel const bernama name menyimpan data string "polinema"
 const name = 'Polinema';
+//  variabel const dioutputkan dengan alert 
 alert(name);
+
+// variabel const digunakan untuk menyimpan data yang bersifat tetap(statis)
+// contohnya const port = "8080"
+// jika port = "3030", baris ini akan error karena nilai const berubah
